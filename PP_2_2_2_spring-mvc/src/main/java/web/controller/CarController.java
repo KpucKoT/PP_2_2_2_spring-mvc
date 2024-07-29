23f5544f7dd91c4b96854334d699f6f7ee031376
KpucKoT <167256393+KpucKoT@users.noreply.github.com>
2024-07-29 22:26:46 +0300

    Add files via upload

--- PP_2_2_2_spring-mvc/src/main/java/web/controller/CarController.java
+++ PP_2_2_2_spring-mvc/src/main/java/web/controller/CarController.java
@@ -1,14 +1,13 @@
 package web.controller;
 
-import model.Car;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import service.CarServiceImpl;
+import web.service.CarService;
+
 
 
 @Controller
@@ -16,10 +15,16 @@ import service.CarServiceImpl;
 public class CarController {
 
 
+    private final CarService carServiceImpl;
+
+    @Autowired
+    public CarController(CarService carServiceImpl) {
+        this.carServiceImpl = carServiceImpl;
+    }
+
 
     @GetMapping("/cars")
         public String cars(@RequestParam(defaultValue = "5") int count, ModelMap model) {
-            CarServiceImpl carServiceImpl = new CarServiceImpl();
             model.addAttribute("cars", carServiceImpl.carList(count));
             return "cars";
 
